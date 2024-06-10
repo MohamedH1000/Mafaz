bf0822e30849be6a9093cb95c0c539c1fe01c60f
MohamedH1000 <mohammedhisham115@gmail.com>
2024-06-11 00:39:36 +0300

    mafaz

--- components/Paginat/Paginat.jsx
+++ components/Paginat/Paginat.jsx
@@ -1,6 +1,15 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import Pagination from "@mui/material/Pagination";
+// import {
+//   Pagination,
+//   PaginationContent,
+//   PaginationEllipsis,
+//   PaginationItem,
+//   PaginationLink,
+//   PaginationNext,
+//   PaginationPrevious,
+// } from "@/components/ui/pagination";
 import { useSearchParams } from "next/navigation";
 import { formUrlQuery } from "@/lib/utils";
 import { useRouter } from "next/navigation";
@@ -23,7 +32,7 @@ const Paginat = ({ resumes }) => {
     router.push(newUrl);
   }, [page]);
   return (
-    <div className="flex justify-center items-center p-4">
+    <div className="flex justify-center items-center p-4" dir="ltr">
       <Pagination
         count={Math.ceil(TotalNumber / pageSize)}
         shape="rounded"
@@ -33,6 +42,22 @@ const Paginat = ({ resumes }) => {
         showLastButton
         color="primary"
       />
+      {/* <Pagination>
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious href="#" />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#">1</PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationEllipsis />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNext href="#" />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination> */}
     </div>
   );
 };
